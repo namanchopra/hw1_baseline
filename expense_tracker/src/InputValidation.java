3194df3d4cff4674f75496fd25fe921a91700859
namanchopra04 <namanchopra04@gmail.com>
2023-09-26 17:31:36 -0400

    Input Validation Checked

---
+++ expense_tracker/src/InputValidation.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.Arrays;
+
+public class InputValidation {
+    
+    private static final List<String> VALID_CATEGORIES = Arrays.asList("food", "travel", "bills", "entertainment", "other");
+
+    public static void validateAmount(double amount) throws IllegalArgumentException {
+        if (amount <= 0 || amount >= 1000) {
+            throw new IllegalArgumentException("Amount must be greater than 0 and less than 1000.");
+        }
+    }
+
+    public static void validateCategory(String category) throws IllegalArgumentException {
+        if (!VALID_CATEGORIES.contains(category.toLowerCase())) {
+            throw new IllegalArgumentException("Invalid category. Please choose from: food, travel, bills, entertainment, other.");
+        }
+    }
+}
